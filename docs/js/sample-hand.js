b9f6388cc713787d505e511a42d56883ea58a3ea
Brendan Hagan <bhaganksu@gmail.com>
2020-09-14 19:44:17 -0400

    fix: basic support for flip cards

--- docs/js/sample-hand.js
+++ docs/js/sample-hand.js
@@ -40,7 +40,11 @@ async function getCardImage(cardName) {
             resolvedImages[lowerCardName] = './img/card_back.jpg';
         } else {
             let json = await response.json();
-            resolvedImages[lowerCardName] = json.image_uris.normal;
+            if (json.card_faces) {
+                resolvedImages[lowerCardName] = json.card_faces[0].image_uris.normal;
+            } else {
+                resolvedImages[lowerCardName] = json.image_uris.normal;   
+            }
         }
     }
 
